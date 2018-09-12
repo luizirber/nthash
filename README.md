ec68525f9bae966bb706c8bebfe317daaf909162
Luiz Irber <luiz.irber@gmail.com>
2018-09-12 13:28:21 -0700

    README and fix tests

---
+++ README.md
@@ -0,0 +1,15 @@
+<meta charset="utf-8"/>
+
+# nthash
+
+**[ntHash](https://github.com/bcgsc/ntHash) implementation in Rust**
+
+[![Build Status](https://travis-ci.org/luizirber/nthash.svg?branch=master)](https://travis-ci.org/luizirber/nthash)
+
+ntHash (version [1.0.4](https://github.com/bcgsc/ntHash/releases/tag/v1.0.4)) implementation in Rust.
+
+This is a work in progress, not published to crates.io yet.
+
+## License
+
+This project is licensed under a [BSD 3-Clause License](LICENSE).
--- src/lib.rs
+++ src/lib.rs
@@ -54,27 +54,27 @@ mod tests {
 
     #[test]
     fn oracle_cmp() {
-        assert_eq!(nthash("TGCAG".as_bytes(), 5), vec![0xbafa6628fc6dab7]);
-        assert_eq!(nthash("ACGTC".as_bytes(), 5), vec![0x480202d34e8ebece]);
+        assert_eq!(nthash("TGCAG".as_bytes(), 5), vec![0xbafa6728fc6dabf]);
+        assert_eq!(nthash("ACGTC".as_bytes(), 5), vec![0x480202d54e8ebecd]);
         assert_eq!(
             nthash("ACGTCGTCAGTCGATGCAGT".as_bytes(), 5),
             vec![
-                0x480202d34e8ebece,
-                0xa997bdd428b4c987,
-                0x8c6d7aa40911b21d,
-                0x5ddcb09590aafeec,
-                0x25ff3ad6bc923826,
-                0x9bda9a443560394a,
-                0x82d449fbb3710cc2,
-                0x1e926cf9780ab81d,
-                0x2f6ed7ac26473a89,
-                0xd1865ec7eb55b03b,
-                0x38b57486189a8af7,
-                0x1b235fddecacf38a,
+                0x480202d54e8ebecd,
+                0xa997bdc628b4c98e,
+                0x8c6d7ab20911b216,
+                0x5ddcb09390aafeef,
+                0x25ff3ac4bc92382f,
+                0x9bda9a5c35603946,
+                0x82d449e5b3710ccd,
+                0x1e926ce7780ab812,
+                0x2f6ed7b226473a86,
+                0xd1865edfeb55b037,
+                0x38b57494189a8afe,
+                0x1b235fc5ecacf386,
                 0x1eab5d82920fda13,
-                0x2c8d1474673bdc5,
-                0xbafa6628fc6dab7,
-                0x14a33bbb28277bec,
+                0x2c8d1574673bdcd,
+                0xbafa6728fc6dabf,
+                0x14a33bb928277bed,
             ]
         )
     }
