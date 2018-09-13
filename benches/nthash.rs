9c148a32b5953dbc1e5c99d2f6686071e38e2b81
Luiz Irber <luiz.irber@gmail.com>
2018-09-12 17:44:19 -0700

    Compare both impls in bench

--- benches/nthash.rs
+++ benches/nthash.rs
@@ -3,51 +3,41 @@ extern crate criterion;
 extern crate nthash;
 extern crate rand;
 
-use criterion::Criterion;
+use criterion::{Bencher, Criterion, Fun};
 use nthash::{nthash, NtHashIterator};
 use rand::distributions::{Distribution, Uniform};
 use rand::thread_rng;
 
-fn nthash_iterator(c: &mut Criterion) {
-    c.bench_function("nthash_iterator", |b| {
-        let range = Uniform::from(0..4);
-        let mut rng = rand::thread_rng();
-        let seq = (0..10000)
-            .map(|_| match range.sample(&mut rng) {
-                0 => 'A',
-                1 => 'C',
-                2 => 'G',
-                3 => 'T',
-                _ => 'N',
-            })
-            .collect::<String>();
+fn nthash_bench(c: &mut Criterion) {
+    let range = Uniform::from(0..4);
+    let mut rng = rand::thread_rng();
+    let seq = (0..10000)
+        .map(|_| match range.sample(&mut rng) {
+            0 => 'A',
+            1 => 'C',
+            2 => 'G',
+            3 => 'T',
+            _ => 'N',
+        })
+        .collect::<String>();
 
+    let nthash_it = Fun::new("nthash_iterator", |b: &mut Bencher, i: &String| {
         b.iter(|| {
-            let iter = NtHashIterator::new(seq.as_bytes(), 5);
-            iter.for_each(drop);
+            let iter = NtHashIterator::new(i.as_bytes(), 5);
+            //  iter.for_each(drop);
+            let res = iter.collect::<Vec<u64>>();
         })
     });
-}
 
-fn nthash_simple(c: &mut Criterion) {
-    c.bench_function("nthash_simple", |b| {
-        let range = Uniform::from(0..4);
-        let mut rng = rand::thread_rng();
-        let seq = (0..10000)
-            .map(|_| match range.sample(&mut rng) {
-                0 => 'A',
-                1 => 'C',
-                2 => 'G',
-                3 => 'T',
-                _ => 'N',
-            })
-            .collect::<String>();
-
+    let nthash_simple = Fun::new("nthash_simple", |b: &mut Bencher, i: &String| {
         b.iter(|| {
-            nthash(seq.as_bytes(), 5);
+            nthash(i.as_bytes(), 5);
         })
     });
+
+    let functions = vec![nthash_it, nthash_simple];
+    c.bench_functions("nthash", functions, seq);
 }
 
-criterion_group!(benches, nthash_iterator, nthash_simple);
+criterion_group!(benches, nthash_bench);
 criterion_main!(benches);
