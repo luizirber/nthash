9ae86b3d5f42a7ce33bd561d8c4c20da10d3155a
Luiz Irber <luiz.irber@gmail.com>
2021-04-13 17:38:28 -0700

    add nix configuration for dev

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+  sources = import ./nix/sources.nix;
+  rustPlatform = import ./nix/rust.nix { inherit sources; };
+  pkgs = import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
+in
+  with pkgs;
+
+  pkgs.mkShell {
+    buildInputs = [
+      rustPlatform.rust.cargo
+      git
+      wasmtime
+      wasm-pack
+      cargo-watch
+      cargo-limit
+    ];
+
+    shellHook = ''
+       # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
+       export SOURCE_DATE_EPOCH=315532800 # 1980
+       export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
+    '';
+  }
