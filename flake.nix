7b1ce81b924c8a3c15a906f284a2e270cbb27921
Luiz Irber <contact+github@luizirber.org>
2024-07-09 18:22:23 -0700

    nix: use flakes

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    rust-flake.url = "github:juspay/rust-flake";
+    rust-flake.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
+      ];
+      perSystem = { config, self', pkgs, lib, ... }: {
+        rust-project.crane.args = {
+          buildInputs = lib.optionals pkgs.stdenv.isDarwin (
+            with pkgs.darwin.apple_sdk.frameworks; [
+              IOKit
+            ]
+          );
+        };
+
+        # Add your auto-formatters here.
+        # cf. https://nixos.asia/en/treefmt
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self'.devShells.nthash ];
+          packages = [
+            pkgs.cargo-watch
+          ];
+        };
+        packages.default = self'.packages.nthash;
+      };
+    };
+}
